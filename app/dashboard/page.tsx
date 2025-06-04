c069cfdb9503d29c675b97394b571890effe5f7b
unknown <tasnifsimanto99@gmail.com>
2025-06-04 17:01:02 +0600

    chart added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar";
-// import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 import { DataTable } from "@/components/data-table";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
@@ -34,7 +34,7 @@ export default function Page() {
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <div className="px-4 lg:px-6">
-                {/* <ChartAreaInteractive /> */}
+                <ChartAreaInteractive />
               </div>
               {Object.entries(processedData).map(([category, metrics]) => (
                 <div key={category} className="mb-8">
